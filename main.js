946af1fe8e79cd5103393e34c1d75672a140b926
Hyo C <neuilly1990@gmail.com>
2015-03-28 03:04:29 -0400

    navigation+work page

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-function Main(){
-	
-}
+	//work;
+	//console.log("hello work!");
+	//object.onclick=function(){myScript};
+//$('#work').click(function(){console.log("hello work!");return false;});
---
+++ work.js
@@ -0,0 +1,9 @@
+function work(){
+	/*var showbox = document.getElementById("showbox");
+
+	var sample1 = new Image();
+	sample1.src = "images/ADPD_photo.jpg";
+
+	showbox.appendChild(sample1);*/
+	console.log("hello work!");
+}
